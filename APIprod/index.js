7e45d4516aff93d57ffb648cd116436cd8d2fa74
Luiz Carlos <d71k1n@live.com>
2022-11-22 10:30:40 -0300

    updated configs and routes

--- APIprod/index.js
+++ APIprod/index.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const PORT = 3002
 
-app.get('/products', (req, res) => res.send('Hello Products API'));
+app.get('/products', (req, res) => res.send('API PROD: works fine!!!'));
+app.get('/products/second', (req, res) => res.send('API PROD: second route works fine!!!'));
 
 app.listen(PORT, () => console.log(`Products API listening on port ${PORT}!`));
--- APIuser/index.js
+++ APIuser/index.js
@@ -1,6 +1,7 @@
 const app = require('express')();
+const PORTS = 3001;
 
-//app.get('/users', (req, res) => res.send('Hello Users API'));
-app.get('/users', (req, res) => res.send('Hello Users API'));
+app.get('/users', (req, res) => res.send('USER API: works fine!!!'));
+app.get('/users/second', (req, res) => res.send('USER API: second route works fine!!!'));
 
-app.listen(3001, () => console.log(`Users API listening on port 3001!`));
+app.listen(3001, () => console.log(`Users API listening on port ${PORTS}!`));
